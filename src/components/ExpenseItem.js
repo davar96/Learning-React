2fb2d1fb463065f39c0da2dae5d67500fbc7b10a
David Arustamyan <david.arustamyan.96@gmail.com>
2022-07-24 15:00:21 +0200

    optimized the date with js functions

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,9 +1,17 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const month = props.date.toLocaleDateString("en-UK", { month: "long" });
+  const day = props.date.toLocaleDateString("en-UK", { month: "2-digit" });
+  const year = props.date.getFullYear();
+
   return (
     <div className="expense-item">
-      <div>{props.date.toLocaleDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
       </div>
