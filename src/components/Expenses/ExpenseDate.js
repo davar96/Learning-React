449a670ebe3fdb18cd67e208fb62b2618946dc61
David Arustamyan <david.arustamyan.96@gmail.com>
2022-07-24 16:32:16 +0200

    organized the project

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,5 +1,6 @@
+import React from "react";
 import "./ExpenseDate.css";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 function ExpenseDate(props) {
   const month = props.date.toLocaleDateString("en-UK", { month: "long" });
